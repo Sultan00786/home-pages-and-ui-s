eff32085a234e51cf7270a5aabe17dd3bd48034e
SultanAliJavedAli <treeloversultan0987@gmail.com>
2025-11-13 20:39:42 +0530

    feat: high level styles.css create in packages/ui which import in apps

--- apps/chroniclehq/app/page.tsx
+++ apps/chroniclehq/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className=" ">Your Great illustrations</h1>
+      <h1 className="heading-58">Your Great illustrations</h1>
       <h1 className="text-3xl">will comes to life here</h1>
     </div>
   );
