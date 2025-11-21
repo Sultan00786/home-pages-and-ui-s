baeb8ca846fe640cb914a6bbea4f61ce576b7b54
SultanAliJavedAli <treeloversultan0987@gmail.com>
2025-11-21 16:44:28 +0530

    feat: done to impelement anivation logic using variants motion

--- packages/ui/src/chroniclehq-ui/home/PanelSlide.tsx
+++ packages/ui/src/chroniclehq-ui/home/PanelSlide.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { motion } from "motion/react";
+import { motion, Variants } from "motion/react";
 import { useState } from "react";
 import { HomeButton } from "../button";
 import { cn } from "../../cn";
+import { pre } from "motion/react-client";
 
 function PanelSlide({
   slideImgs,
@@ -12,29 +13,50 @@ function PanelSlide({
   slideImgs: string[];
   uiToolbar: string;
 }) {
-  const [currentSlide, setCurrentSlide] = useState(5);
+  const [currentSlide, setCurrentSlide] = useState(0);
   const [previousSlide, setPreviousSlide] = useState<null | number>(null);
+  const [animationState, setAnimationState] = useState<
+    "nextSlide" | "prevSlid" | ""
+  >("");
+  const currentSlideAnimation: Variants = {
+    nextSlide: {
+      translateY: 800,
+    },
+    prevSlid: {
+      translateY: -800,
+    },
+  };
+  const previousSlideAnimation: Variants = {
+    nextSlide: {
+      translateY: -800,
+    },
+    prevSlid: {
+      translateY: 800,
+    },
+  };
   return (
     <div className="">
       <div className="lg-panelSlide:w-[1192px] w-full h-full lg-panelSlide:h-[728px] bg-black-500/10 px-[70px] relative rounded-[0.8rem] border-4 border-black-900/10 overflow-hidden">
         <div className=" h-full relative z-0 flex-col gap-y-[60px]">
-          {currentSlide && (
+          {(currentSlide || currentSlide === 0) && (
             <motion.img
-              initial={{ translateY: 800 }}
-              animate={{ translateY: 0 }}
+              initial={!previousSlide ? false : animationState}
+              animate={!previousSlide ? false : { translateY: 0 }}
               // exit={{ translateY: -800 }}          didn't work
               transition={{ duration: 0.35, ease: "easeInOut", delay: 0.3 }}
+              variants={currentSlideAnimation}
               src={slideImgs[currentSlide]}
               alt={`slide-${currentSlide + 1}`}
               key={`current-slide-${currentSlide + 1}`}
               className="w-full rounded-[calc(0.8rem-4px)] absolute inset-0 m-auto"
             />
           )}
-          {previousSlide && (
+          {(previousSlide || previousSlide === 0) && (
             <motion.img
-              initial={{ translateY: 0 }}
-              animate={{ translateY: -800 }}
+              initial={!previousSlide ? false : { translateY: 0 }}
+              animate={!previousSlide ? false : animationState}
               transition={{ duration: 0.35, ease: "easeInOut", delay: 0.3 }}
+              variants={previousSlideAnimation}
               src={slideImgs[previousSlide]}
               alt={`slide-${previousSlide + 1}`}
               key={`previous-slide-${currentSlide + 1}`}
@@ -50,15 +72,26 @@ function PanelSlide({
           />
         </div>
       </div>
-      <HomeButton
+      {/* <HomeButton
         onClick={() => {
+          const slideIndex = Math.floor(Math.random() * 8);
+          if (slideIndex > currentSlide) setAnimationState("nextSlide");
+          else if (slideIndex < currentSlide) setAnimationState("prevSlid");
+          else setAnimationState("");
           setPreviousSlide(currentSlide);
-          setCurrentSlide(Math.floor(Math.random() * 8));
+          setCurrentSlide(slideIndex);
         }}
         variant="primary"
       >
         click
-      </HomeButton>
+      </HomeButton> */}
+      <div className="w-full py-4 flex gap-2 items-center justify-center">
+        {slideImgs.map((_, index) => (
+          <div key={`slide-tab-${index + 1}`} className="ho">
+            slide {index + 1}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
