61cc9ba1ea209262d7d233eb94733e5459a5e79d
SultanAliJavedAli <treeloversultan0987@gmail.com>
2025-12-01 20:05:06 +0530

    fix: hieght issue

--- packages/ui/src/chroniclehq-ui/home/Testimonial.tsx
+++ packages/ui/src/chroniclehq-ui/home/Testimonial.tsx
@@ -6,7 +6,7 @@ export default function Testimonial({ index }: { index: number }) {
     <div
       key={index}
       className={cn(
-        "min-w-[320px] h-80 flex-1 bg-black-500/15 rounded-lg border-black-900/30 border"
+        "min-w-[320px] min-h-80 flex-1 bg-black-500/15 rounded-lg border-black-900/30 border"
       )}
     >
       <h1 className="text-white">Testimonial {index + 1}</h1>
